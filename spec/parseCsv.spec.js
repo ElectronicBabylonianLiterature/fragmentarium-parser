b0f54fb54915611da5e47420116e7d61f62287ce
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-07-02 17:24:58 +0200

    Parse joins to an array

--- spec/parseCsv.spec.js
+++ spec/parseCsv.spec.js
@@ -1,43 +1,59 @@
 const parseCsv = require('../src/parseCsv')
 
+const csvDefaults = {
+  _id: '_id',
+  cdliNumber: 'cdliNumber',
+  bmidNumber: 'bmidNumber',
+  accession: 'accession',
+  genre: 'genre',
+  fincke: 'fincke',
+  publicationPlace: 'publicationPlace',
+  joins: '',
+  subcollection: 'subcollection',
+  description: 'description',
+  length: 'length',
+  width: 'width',
+  thickness: 'thickness',
+  collection: 'collection',
+  script: 'script',
+  date: 'date',
+  folio: 'folio',
+  register: 'register',
+  transliteration: 'transliteration',
+  notes: 'notes'
+}
+
+const objectDefaults = {
+  ...csvDefaults,
+  joins: []
+}
+
 function buildObject (params) {
-  const defaults = {
-    _id: '_id',
-    cdliNumber: 'cdliNumber',
-    bmidNumber: 'bmidNumber',
-    accession: 'accession',
-    genre: 'genre',
-    fincke: 'fincke',
-    publicationPlace: 'publicationPlace',
-    joins: 'joins',
-    subcollection: 'subcollection',
-    description: 'description',
-    length: 'length',
-    width: 'width',
-    thickness: 'thickness',
-    collection: 'collection',
-    script: 'script',
-    date: 'date',
-    folio: 'folio',
-    register: 'register',
-    transliteration: 'transliteration',
-    notes: 'notes'
-  }
   return {
-    ...defaults,
+    ...objectDefaults,
     ...params
   }
 }
 
-function buildLine (object) {
-  return `"${object._id}","${object.cdliNumber}","${object.bmidNumber}","${object.accession}","${object.genre}","${object.fincke}","${object.publicationPlace}","${object.joins}","${object.subcollection}","${object.description}","${object.length}",${object.width}","${object.thickness}","${object.collection}","${object.script}","${object.date}","${object.folio}","${object.register}","${object.transliteration}","${object.notes}"`
+function buildCsv (params) {
+  const object = {
+    ...csvDefaults,
+    ...params
+  }
+  return `_id,cdliNumber,bmidNumber,accession,genre,fincke,publicationPlace,joins,subcollection,description,length,width,thickness,collection,script,date,folio,register,transliteration,notes
+"${object._id}","${object.cdliNumber}","${object.bmidNumber}","${object.accession}","${object.genre}","${object.fincke}","${object.publicationPlace}","${object.joins}","${object.subcollection}","${object.description}","${object.length}","${object.width}","${object.thickness}","${object.collection}","${object.script}","${object.date}","${object.folio}","${object.register}","${object.transliteration}","${object.notes}"`
 }
 
 describe('parseCsv', () => {
   it('parses all columns', () => {
-    const expected = buildObject()
-    const csv = `_id,cdliNumber,bmidNumber,accession,genre,fincke,publicationPlace,joins,subcollection,description,length,width,thickness,collection,script,date,folio,register,transliteration,notes
-${buildLine(expected)}`
+    const csv = buildCsv({})
+    const expected = buildObject({})
+    expect(parseCsv(csv)).toEqual([expected])
+  })
+
+  it('parses + joins', () => {
+    const csv = buildCsv({joins: 'join1 + join2'})
+    const expected = buildObject({joins: ['join1', 'join2']})
     expect(parseCsv(csv)).toEqual([expected])
   })
 })
--- src/parseCsv.js
+++ src/parseCsv.js
@@ -1,5 +1,8 @@
 const Papa = require('papaparse')
 
 module.exports = function parseCsv (csv) {
-  return Papa.parse(csv, {header: true}).data
+  return Papa.parse(csv, {header: true}).data.map(rawObject => ({
+    ...rawObject,
+    joins: rawObject.joins ? rawObject.joins.split(' + ') : []
+  }))
 }
